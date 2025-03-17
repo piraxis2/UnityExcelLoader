004d7966303c17d787f9bbe37870213731ec5109
piraxis <kate107852@gmail.com>
2025-03-17 14:09:06 +0900

    샘플 메타파일 추가

---
+++ Example/Sample.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityExcelLoader.Runtime;
+
+//DoNotRemove CustomUsing Start End Annotation
+//CustomUsingStart
+
+
+//CustomUsingEnd
+namespace UnityExcelLoader
+{
+	[ExcelScriptableObject]
+	public class Sample : ScriptableObject
+	{
+		public List< Sample_Entity > Data;
+//DoNotRemove Methods Start End Annotation
+//MethodsStart
+		
+//MethodsEnd
+	}
+}
---
+++ Example/Sample_Entity.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+//DoNotRemove CustomUsing Start End Annotation
+//CustomUsingStart
+
+//CustomUsingEnd
+
+namespace UnityExcelLoader
+{
+	[Serializable]
+	public class Sample_Entity
+	{
+			public int Id;
+		public string Name;
+		public float Chance;
+		public bool IsUse;
+
+
+//DoNotRemove Methods Start End Annotation
+//MethodsStart
+		
+//MethodsEnd
+	}
+}
