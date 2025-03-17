3e9691f7079324a76fae6102bf463b3707a39f9c
piraxis2 <kate107852@gmail.com>
2025-03-18 01:33:11 +0900

    임포트 방식에서 create 방식으로 변경

--- Editor/UnityExcelLoaderMenuItems.cs
+++ Editor/UnityExcelLoaderMenuItems.cs
@@ -1,5 +1,7 @@
 ﻿using System.IO;
 using System;
+using System.Collections;
+using System.Collections.Generic;
 using System.Text;
 using NPOI.HSSF.UserModel;
 using NPOI.SS.UserModel;
@@ -9,38 +11,58 @@ using UnityEditor;
 
 namespace UnityExcelLoader.Editor
 {
+
+    public struct ExcelDirectoryData
+    {
+        public string ExcelPath;
+        public string ExcelName;
+        public string SavePath;
+    }
     public class UnityExcelLoaderMenuItems
     {
         [MenuItem("Assets/Create/UnityExcelLoader/ExcelScript", false)]
-        private static void ExcelToScriptableObject()
+        private static void ExcelToScript()
         {
-            var savePath = EditorUtility.SaveFolderPanel("Save ExcelAssetScript", Application.dataPath, "");
-            if (string.IsNullOrEmpty(savePath)) return;
+            ExcelDirectoryData excelDirectoryData = GetExcelDirectoryData("Save ExcelScript");
 
-            var selectedAssets = Selection.GetFiltered(typeof(UnityEngine.Object), SelectionMode.Assets);
-            var excelPath = AssetDatabase.GetAssetPath(selectedAssets[0]);
-            var excelName = Path.GetFileNameWithoutExtension(excelPath);
+            if (string.IsNullOrEmpty(excelDirectoryData.SavePath)) return;
 
-            CreateEntity(savePath, excelPath, excelName);
-            CreateScript(savePath, excelName);
+            CreateEntity(excelDirectoryData);
+            CreateScript(excelDirectoryData);
             AssetDatabase.Refresh();
         }
 
         [MenuItem("Assets/Create/UnityExcelLoader/ExcelScript", true)]
         private static bool IsValidateCreateExcelScript() => IsExcel();
+        
+        
+        [MenuItem("Assets/Create/UnityExcelLoader/ExcelScriptableObject", false)]
+        private static void ExcelToScriptableObject()
+        {
+            ExcelDirectoryData excelDirectoryData = GetExcelDirectoryData("Save ExcelScriptableObject");
 
-        private static void CreateScript(string savePath, string excelName)
+            if (string.IsNullOrEmpty(excelDirectoryData.SavePath)) return;
+
+            CreateScriptableObject(excelDirectoryData);
+            AssetDatabase.Refresh();
+        }
+        
+        [MenuItem("Assets/Create/UnityExcelLoader/ExcelScriptableObject", true)]
+        private static bool IsValidateCreateExcelScriptableObject() => IsExcel();
+
+
+        private static void CreateScript(ExcelDirectoryData excelDirectoryData)
         {
             const string templatePath = "ScriptTemplate.txt";
             var filePath = Directory.GetFiles(Directory.GetCurrentDirectory(), templatePath, SearchOption.AllDirectories);
             if (filePath.Length == 0) throw new Exception("Script template not found.");
 
             var templateContent = File.ReadAllText(filePath[0]);
-            var scriptPath = Path.Combine(savePath, $"{excelName}.cs");
+            var scriptPath = Path.Combine(excelDirectoryData.SavePath, $"{excelDirectoryData.ExcelName}.cs");
 
             var classContent = templateContent
-                .Replace("{ClassName}", excelName)
-                .Replace("{Entity}", $"{excelName}_Entity");
+                .Replace("{ClassName}", excelDirectoryData.ExcelName)
+                .Replace("{Entity}", $"{excelDirectoryData.ExcelName}_Entity");
 
             if (File.Exists(scriptPath))
             {
@@ -66,10 +88,10 @@ namespace UnityExcelLoader.Editor
             File.WriteAllText(scriptPath, classContent);
         }
 
-        private static void CreateEntity(string savePath, string excelPath, string excelName)
+        private static void CreateEntity(ExcelDirectoryData excelDirectoryData)
         {
-            using var stream = File.Open(excelPath, FileMode.Open, FileAccess.Read, FileShare.ReadWrite);
-            IWorkbook book = Path.GetExtension(excelPath) == ".xls" ? new HSSFWorkbook(stream) : new XSSFWorkbook(stream);
+            using var stream = File.Open(excelDirectoryData.ExcelPath, FileMode.Open, FileAccess.Read, FileShare.ReadWrite);
+            IWorkbook book = Path.GetExtension(excelDirectoryData.ExcelPath) == ".xls" ? new HSSFWorkbook(stream) : new XSSFWorkbook(stream);
             const string templatePath = "EntityTemplate.txt";
             var filePath = Directory.GetFiles(Directory.GetCurrentDirectory(), templatePath, SearchOption.AllDirectories);
             if (filePath.Length == 0) throw new Exception("Script template not found.");
@@ -79,7 +101,7 @@ namespace UnityExcelLoader.Editor
             {
             	var templateContent = File.ReadAllText(filePath[0]);
                 var sheet = book.GetSheetAt(i);
-                var entityPath = Path.Combine(savePath, $"{excelName}_Entity.cs");
+                var entityPath = Path.Combine(excelDirectoryData.SavePath, $"{excelDirectoryData.ExcelName}_Entity.cs");
 
                 var headerRow = sheet.GetRow(0);
                 var typeRow = sheet.GetRow(1);
@@ -92,14 +114,14 @@ namespace UnityExcelLoader.Editor
                     
                     if (headerCell.StringCellValue.StartsWith("#")) continue;
 
-                    if (headerCell != null && headerCell.CellType != CellType.Blank && typeCell != null && typeCell.CellType != CellType.Blank)
+                    if (headerCell.CellType != CellType.Blank && typeCell != null && typeCell.CellType != CellType.Blank)
                     {
                         fieldsBuilder.AppendLine($"\t\tpublic {typeCell.StringCellValue} {headerCell.StringCellValue};");
                     }
                 }
 
                 var classContent = templateContent
-                    .Replace("{ClassName}", $"{excelName}_Entity")
+                    .Replace("{ClassName}", $"{excelDirectoryData.ExcelName}_Entity")
                     .Replace("{Fields}", fieldsBuilder.ToString());
 
                 if (File.Exists(entityPath))
@@ -127,12 +149,106 @@ namespace UnityExcelLoader.Editor
             }
         }
 
+        private static void CreateScriptableObject(ExcelDirectoryData excelDirectoryData)
+        {
+            var unityPath = excelDirectoryData.SavePath[excelDirectoryData.SavePath.LastIndexOf("Assets", StringComparison.Ordinal)..];
+            var scriptableObjectPath = unityPath + $"/{excelDirectoryData.ExcelName}.asset";
+            var entityType = Type.GetType($"UnityExcelLoader.{excelDirectoryData.ExcelName}_Entity, Assembly-CSharp");
+
+            if (entityType == null)
+            {
+                Debug.LogError($"Entity type {excelDirectoryData.ExcelName}_Entity not found.");
+                return;
+            }
+
+            var scriptableObject = ScriptableObject.CreateInstance(excelDirectoryData.ExcelName);
+            scriptableObject.hideFlags = HideFlags.NotEditable;
+            var dataList = (IList)Activator.CreateInstance(typeof(List<>).MakeGenericType(entityType));
+
+            using var stream = File.Open(excelDirectoryData.ExcelPath, FileMode.Open, FileAccess.Read, FileShare.ReadWrite);
+            IWorkbook book = Path.GetExtension(excelDirectoryData.ExcelPath) == ".xls" ? new HSSFWorkbook(stream) : new XSSFWorkbook(stream);
+
+            for (int i = 0; i < book.NumberOfSheets; i++)
+            {
+                var sheet = book.GetSheetAt(i);
+                var headerRow = sheet.GetRow(0);
+                
+                for (int j = 2; j <= sheet.LastRowNum; j++)
+                {
+                    var row = sheet.GetRow(j);
+                    var frontCell = row.GetCell(0);
+
+                    if (frontCell.CellType == CellType.String && frontCell.StringCellValue.StartsWith("#")) continue;
+                    
+                    var entity = Activator.CreateInstance(entityType);
+
+                    for (int k = 0; k < headerRow.LastCellNum; k++)
+                    {
+                        var headerCell = headerRow.GetCell(k);
+                        
+                        if (headerCell.StringCellValue.StartsWith("#")) continue;
+                        
+                        var dataCell = row.GetCell(k);
+
+                        if (dataCell == null) continue;
+                        
+                        var fieldName = headerCell.StringCellValue;
+                        var field = entityType.GetField(fieldName);
+                        
+                        if (field == null) continue;
+
+                        if (string.IsNullOrEmpty(dataCell.ToString())) continue;
+                        
+                        try
+                        {
+                            var value = Convert.ChangeType(dataCell.ToString(), field.FieldType);
+                            field.SetValue(entity, value);
+                        }
+                        catch (InvalidCastException)
+                        {
+                            throw new InvalidCastException($"Cannot convert '{dataCell}' to {field.FieldType.Name} for property '{fieldName}'.");
+                        }
+                        catch (FormatException)
+                        {
+                            throw new FormatException($"'{dataCell}' is not in a valid format for property '{fieldName}'.");
+                        }
+                        catch (OverflowException)
+                        {
+                            throw new OverflowException($"'{dataCell}' is not in a valid format for property '{fieldName}'.");
+                        }
+                        catch (Exception ex)
+                        {
+                            throw new Exception($"An error occurred while setting property '{fieldName}': {ex.Message}");
+                        }
+                    }
+
+                    dataList.Add(entity);
+                }
+            }
+
+            var fieldInfo = scriptableObject.GetType().GetField("Data");
+            fieldInfo?.SetValue(scriptableObject, dataList);
+
+            AssetDatabase.CreateAsset(scriptableObject, scriptableObjectPath);
+            EditorUtility.SetDirty(scriptableObject);
+        }
+
         private static bool IsExcel()
         {
             var selectedAssets = Selection.GetFiltered(typeof(UnityEngine.Object), SelectionMode.Assets);
             if (selectedAssets.Length != 1) return false;
             var path = AssetDatabase.GetAssetPath(selectedAssets[0]);
             return Path.GetExtension(path) == ".xls" || Path.GetExtension(path) == ".xlsx";
+        }
+
+        private static ExcelDirectoryData GetExcelDirectoryData(string saveFolderTitle)
+        {
+            var selectedAssets = Selection.GetFiltered(typeof(UnityEngine.Object), SelectionMode.Assets);
+            var excelPath = AssetDatabase.GetAssetPath(selectedAssets[0]);
+            var excelName = Path.GetFileNameWithoutExtension(excelPath);
+            var initialDirectory = Path.GetDirectoryName(excelPath);
+            var savePath = EditorUtility.SaveFolderPanel(saveFolderTitle, initialDirectory, "");
+            return new ExcelDirectoryData { ExcelPath = excelPath, ExcelName = excelName, SavePath = savePath };
         }
     }
 }
---
+++ Editor/Utility.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace UnityExcelLoader.Editor
+{
+    public class Utility
+    {
+        public static string ConvertToUnityAssetPath(string fullPath)
+        {
+            string dataPath = Application.dataPath.Replace("/", "\\");
+            if (fullPath.StartsWith(dataPath))
+            {
+                return "Assets" + fullPath.Substring(dataPath.Length);
+            }
+
+            Debug.LogError("The provided path is not within the Assets folder.");
+            return null;
+        } 
+    }
+}
